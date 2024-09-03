20b19b5df99a5de309871e86ebeaf5c4b204fee5
Thiago Silva <smt.thiago99@gmail.com>
2024-09-03 14:39:34 -0300

    Revert "Merge branch 'develop' into thiago"
    This reverts commit b860309bd087d5fcb83d14391bd1a067943d12d7, reversing
    changes made to 6ece075e3f9c3fb52d2035a374dfe956c12fd372.

--- src/routes/tickets.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const ticketsRouter = Router();
-
-// Exemplos - pode apagar e fazer corretamente
-//ticketsRouter.get(...)
-//ticketsRouter.pust(...)
-
-export default ticketsRouter;

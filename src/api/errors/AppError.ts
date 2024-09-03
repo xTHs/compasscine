9523df9875b96f4b4b17da4350dd35dcbda0f8da
Walmircsanto <walmirsantosrosa@gmail.com>
2024-09-03 15:56:05 -0300

    feat(AppError) add new code parameter in AppError

--- src/api/errors/AppError.ts
+++ src/api/errors/AppError.ts
@@ -1,10 +1,12 @@
 class AppError {
   public readonly message: string;
-  public readonly statusCode: number;
+  public readonly status: string;
+  public readonly code: number;
 
-  constructor(message: string, statusCode = 400) {
+  constructor(message: string, status: string, code = 400) {
     this.message = message;
-    this.statusCode = statusCode;
+    this.status = status;
+    this.code = code;
   }
 }
 

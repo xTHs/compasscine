ddf86d56a30832790f58d5539dfaecf1b9151e15
Sthephanny Jamilly <sthephanny.jrv@gmail.com>
2024-09-02 22:01:53 -0300

    feat: creating main files and connecting to database

---
+++ src/routes/movies.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const moviesRouter = Router();
+
+// Exemplos - pode apagar e fazer corretamente
+//moviesRouter.get(...)
+//moviesRouter.put(...)
+
+export default moviesRouter;

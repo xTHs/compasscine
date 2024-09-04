2e75a068edc3c4eb2da0d1717565d3c1a7b9e530
Walmircsanto <walmirsantosrosa@gmail.com>
2024-09-04 16:04:19 -0300

    feat(CreateSessao): implements migration methods to create and drop tables

--- src/api/shared/typeorm/migrations/1725385716443-CreateSessao.ts
+++ src/api/shared/typeorm/migrations/1725385716443-CreateSessao.ts
@@ -1,11 +1,49 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateSessao1725385716443 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'sessao',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'room',
+            type: 'varchar',
+          },
+          {
+            name: 'capacity',
+            type: 'int',
+          },
+          {
+            name: 'day',
+            type: 'timestamp',
+            default: 'now',
+          },
+          {
+            name: 'time',
+            type: 'datatime',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('sessao');
+  }
 }

29182325b879dfa2f11ab3903d58cd9855e84b83
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-09-04 14:25:27 -0300

    refactor

--- src/routes/sessions.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const sessionsRouter = Router();
-
-// Exemplos - pode apagar e fazer corretamente
-//sessionsRouter.get(...)
-//sessionsRouter.put(...)
-
-export default sessionsRouter;

f096a7ebd59982dd1bf7476e73562708a4eef7b1
Walmircsanto <walmirsantosrosa@gmail.com>
2024-09-05 13:46:45 -0300

    feat: create CreateSessionService

---
+++ src/api/modules/sessions/services/CreateSessionService.ts
@@ -0,0 +1,52 @@
+import { getCustomRepository } from 'typeorm';
+import Session from '../typeorm/entities/Session';
+import SessionRepository from '../typeorm/repositories/SessionRepository';
+import MoviesRepository from '../../movies/typeorm/repositories/MoviesRepository';
+import AppError from 'src/api/shared/errors/AppError';
+import Movie from '../../movies/typeorm/entities/Movie';
+
+interface IRequest {
+  room: string;
+  capacity: number;
+  day: Date;
+  time: Date;
+}
+interface moviedID {
+  movie_id: number;
+}
+
+class CreateSessionService {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  public async execute(
+    { room, capacity, day, time }: IRequest,
+    { movie_id }: moviedID,
+  ) {
+    const moveiId = movie_id;
+    const sessionRepository = getCustomRepository(SessionRepository);
+
+    // verifica se ja existe sala com esse nome
+    const roomExisted = await sessionRepository.findByRoom(room);
+
+    const moviesRepository = getCustomRepository(MoviesRepository);
+
+    //const movie = await moviesRepository.findOne(movie_id, {
+    // relations: ['sessions'],
+    const movie = await moviesRepository.findById(moveiId);
+
+    if (roomExisted && movie === null) {
+      throw new AppError('The room has already registered', 'bad request', 400);
+    }
+
+    const session = sessionRepository.create({
+      room,
+      capacity,
+      day,
+      time,
+    });
+
+    await sessionRepository.save(session);
+    return session;
+  }
+}
+
+export default CreateSessionService;

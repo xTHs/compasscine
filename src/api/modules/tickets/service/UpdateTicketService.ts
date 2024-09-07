a6de467da2ecf2bed58f43513f43d8d570216686
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-09-07 17:01:42 -0300

    fix
    Correction of ticket update functions. The function did not have a correct relation between session and the movieId column

--- src/api/modules/tickets/service/UpdateTicketService.ts
+++ src/api/modules/tickets/service/UpdateTicketService.ts
@@ -4,29 +4,26 @@ import Ticket from '../typeorm/entities/Ticket';
 import TicketsRepository from '../typeorm/repositories/TicketsRepository';
 
 interface IRequest {
-  id: number;
   chair: string;
   value: number;
+}
+interface IReqParam {
+  id: number;
   session_id: number;
   movie_id: number;
 }
 
 class UpdateTicketService {
-  public async execute({
-    id,
-    movie_id,
-    session_id,
-    value,
-    chair,
-  }: IRequest): Promise<Ticket> {
+  public async execute(
+    { chair, value }: IRequest,
+    { id, session_id, movie_id }: IReqParam,
+  ): Promise<Ticket> {
     const ticketsRepository = getCustomRepository(TicketsRepository);
 
     const ticket = await ticketsRepository.findTicketforUpdateAndDelete(
       id,
       movie_id,
       session_id,
-      value,
-      chair,
     );
 
     if (!ticket) {

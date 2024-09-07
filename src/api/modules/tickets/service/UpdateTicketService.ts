c29ebaa7fc70ee2487278cd0810f9768e927911a
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-09-07 17:47:33 -0300

    fix

---
+++ src/api/modules/tickets/service/UpdateTicketService.ts
@@ -0,0 +1,54 @@
+import AppError from 'src/api/shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Ticket from '../typeorm/entities/Ticket';
+import TicketsRepository from '../typeorm/repositories/TicketsRepository';
+
+interface IRequest {
+  chair: string;
+  value: number;
+}
+interface IReqParam {
+  id: number;
+  session_id: number;
+  movie_id: number;
+}
+
+class UpdateTicketService {
+  public async execute(
+    { chair, value }: IRequest,
+    { id, session_id, movie_id }: IReqParam,
+  ): Promise<Ticket> {
+    const ticketsRepository = getCustomRepository(TicketsRepository);
+
+    const ticket = await ticketsRepository.findTicketforUpdateAndDelete(
+      id,
+      movie_id,
+      session_id,
+    );
+
+    if (!ticket) {
+      throw new AppError(`Ticket não encontrado`, 'Not Found', 404);
+    }
+
+    const chairSessionExists = await ticketsRepository.findByChairAndSession(
+      session_id,
+      chair,
+    );
+
+    if (chairSessionExists && chair !== ticket.chair) {
+      throw new AppError(
+        `O assento ${chair} já foi reservado para esta sessão.`,
+        'Bad Request',
+      );
+    }
+
+    ticket.chair = chair;
+    ticket.value = value;
+
+    await ticketsRepository.save(ticket);
+
+    return ticket;
+  }
+}
+
+export default UpdateTicketService;

94502033539f1a1e374c76e350167996bd82165f
Walmircsanto <walmirsantosrosa@gmail.com>
2024-09-09 13:12:34 -0300

    feat: change from when deleting eithout action to when deleting in cascade

--- src/api/shared/typeorm/migrations/1725652954450-RelationTicketsSessions.ts
+++ src/api/shared/typeorm/migrations/1725652954450-RelationTicketsSessions.ts
@@ -5,7 +5,7 @@ export class RelationTicketsSessions1725652954450 implements MigrationInterface 
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "tickets" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "chair" varchar NOT NULL, "value" integer NOT NULL, "created_at" datetime NOT NULL DEFAULT (datetime('now')), "updated_at" datetime NOT NULL DEFAULT (datetime('now')), "sessionId" integer)`);
-        await queryRunner.query(`CREATE TABLE "temporary_tickets" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "chair" varchar NOT NULL, "value" integer NOT NULL, "created_at" datetime NOT NULL DEFAULT (datetime('now')), "updated_at" datetime NOT NULL DEFAULT (datetime('now')), "sessionId" integer, CONSTRAINT "FK_d175b024857bfa9676f6a06368f" FOREIGN KEY ("sessionId") REFERENCES "sessions" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
+        await queryRunner.query(`CREATE TABLE "temporary_tickets" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "chair" varchar NOT NULL, "value" integer NOT NULL, "created_at" datetime NOT NULL DEFAULT (datetime('now')), "updated_at" datetime NOT NULL DEFAULT (datetime('now')), "sessionId" integer, CONSTRAINT "FK_d175b024857bfa9676f6a06368f" FOREIGN KEY ("sessionId") REFERENCES "sessions" ("id") ON DELETE CASCADE  ON UPDATE NO ACTION)`);
         await queryRunner.query(`INSERT INTO "temporary_tickets"("id", "chair", "value", "created_at", "updated_at", "sessionId") SELECT "id", "chair", "value", "created_at", "updated_at", "sessionId" FROM "tickets"`);
         await queryRunner.query(`DROP TABLE "tickets"`);
         await queryRunner.query(`ALTER TABLE "temporary_tickets" RENAME TO "tickets"`);

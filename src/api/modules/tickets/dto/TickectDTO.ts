40c2bef1acfb7c44bbed7bbabeb3e5df9b229cc1
Walmircsanto <walmirsantosrosa@gmail.com>
2024-09-09 13:10:22 -0300

    feat: add method to convert ticket vector to a ticket vectorDTO

--- src/api/modules/tickets/dto/TickectDTO.ts
+++ src/api/modules/tickets/dto/TickectDTO.ts
@@ -1,3 +1,4 @@
+import { Any } from 'typeorm';
 import Ticket from '../typeorm/entities/Ticket';
 
 class TicketDTO {
@@ -5,12 +6,23 @@ class TicketDTO {
   session_id?: number;
   chair: string;
   value: number;
-
   constructor(ticket: Ticket) {
     this.id = ticket.id;
     this.session_id = ticket.session?.id;
     this.chair = ticket.chair;
     this.value = ticket.value;
+  }
+
+  static convertTickInTicktDTO(tickets: Ticket[]) {
+    if (!tickets) {
+      return [];
+    } else {
+      const ticktesDTO: TicketDTO[] = [];
+      for (let i = 0; i < tickets.length; i++) {
+        ticktesDTO.push(new TicketDTO(tickets[i]));
+      }
+      return ticktesDTO;
+    }
   }
 }
 
